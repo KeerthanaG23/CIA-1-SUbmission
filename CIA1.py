57b5ae4ff1d7b92f7645ad39dff7dc473ea6fa71
Keerthana G <116378322+KeerthanaG23@users.noreply.github.com>
2023-04-05 18:50:18 +0530

    Update CIA1.py

--- CIA1.py
+++ CIA1.py
@@ -7,8 +7,6 @@ import string
 
 sequence=['A','T','G','C']
 
-
-
 def randStr(chars = string.ascii_uppercase + string.digits, N=16):
 	return ''.join(random.choice(chars) for _ in range(N))
 string_1=randStr(chars='ATGC')
@@ -25,13 +23,64 @@ mismatch_score=-4
 l1=len(string_1)
 l2=len(string_2)
 
+
+
+seq1 = 'ATCGATCGATCG'
+seq2 = 'ATCGATAG'
 #Matrix creation initialised to zeros
 
+#Scoring function
+
+def score(i, j):
+    if i == 0 and j == 0:
+        return 0
+    elif i == 0:
+        return j * mismatch_score
+    elif j == 0:
+        return i * mismatch_score
+    else:
+        match = score(i-1, j-1) + (match_score if seq1[i-1] == seq2[j-1] else mismatch_score)
+        delete = score(i-1, j) + mismatch_score
+        insert = score(i, j-1) + mismatch_score
+        return max(match, delete, insert)
 
 
-import numpy as np
+m, n = len(seq1), len(seq2)
+matrix = np.zeros((m+1, n+1), dtype=int)
+for i in range(m+1):
+    for j in range(n+1):
+        matrix[i,j] = score(i, j)
+	
+	
+#Backtracking
+def backtrack(i, j):
+    if i == 0 and j == 0:
+        return '', ''
+    elif i == 0:
+        s1, s2 = backtrack(i, j-1)
+        return s1 + '-', s2 + seq2[j-1]
+    elif j == 0:
+        s1, s2 = backtrack(i-1, j)
+        return s1 + seq1[i-1], s2 + '-'
+    else:
+        match = score(i-1, j-1) + (match_score if seq1[i-1] == seq2[j-1] else mismatch_score)
+        delete = score(i-1, j) + mismatch_score
+        insert = score(i, j-1) + mismatch_score
+        if matrix[i,j] == match:
+            s1, s2 = backtrack(i-1, j-1)
+            return s1 + seq1[i-1], s2 + seq2[j-1]
+        elif matrix[i,j] == delete:
+            s1, s2 = backtrack(i-1, j)
+            return s1 + seq1[i-1], s2 + '-'
+        else:
+            s1, s2 = backtrack(i, j-1)
+            return s1 + '-', s2 + seq2[j-1]
+alignment1, alignment2 = backtrack(m, n)	
+#import numpy as np
 
-alignment_mat=np.zeros((5,5))
-print(alignment_mat)
-
-max_score=0 
+#alignment_mat=np.zeros((5,5))
+#print(alignment_mat)
+print("Alignment score:", matrix[m,n])
+print("Alignment:")
+print(alignment1)
+print(alignment2)
